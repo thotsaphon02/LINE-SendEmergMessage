5116e63c1bc4728bcaa6952813f9306dd8229be2
thotsaphon02 <55377462+thotsaphon02@users.noreply.github.com>
2019-10-15 15:51:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	date_default_timezone_set("Asia/Bangkok");
 
 	$sToken = "C3bnBm2FevZAVCXFkA7LYNnKwPQ70zSinHjKy4yFNmT";
-	$sMessage = $data
+	$sMessage = $data;
 	
 	$chOne = curl_init(); 
 	curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
